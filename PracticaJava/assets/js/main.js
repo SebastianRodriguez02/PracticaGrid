5f24c92982d4d971676006e6ac6b3acc0f909061
SebastianRodriguez02 <sebastian3696@gmail.com>
2022-11-29 18:16:44 -0500

    29

--- PracticaJava/assets/js/main.js
+++ PracticaJava/assets/js/main.js
@@ -1,11 +1,32 @@
 function Completado() {
 
     let Nombres = document.getElementById("NombreUsuarios").value
-    if (Nombres ="") {
-        console.log(`Formulario completado`)
-        let cajaP = document.getElementById("saludoUsuario")
-        cajaP.innerHTML = `Hola! ${nombreUsuario}, <span class="text-danger fw-bold">como estas?</span>`
-    } else {
-        alert("Complete la casilla de nombre")
+    if (Nombres =="") {       
+       alert("Complete la casilla de nombre")
+       return
+    }
+
+    let Apellidos = document.getElementById("ApellidoUsuarios").value
+    if (Apellidos =="") {
+        alert("Complete la casilla de apellido")
+        return
+    }
+
+    let Correoelectronico = document.getElementById("ApellidoUsuarios").value
+    if (Correoeletronico =="") {
+        alert("Complete la casilla de correo")
+        return
+    }
+
+    let Ciudad = document.getElementById("ApellidoUsuarios").value
+    if (ciudad =="") {
+        alert("Complete la casilla de ciudad")
+        return
+    }
+
+    let Paises = document.getElementById("Paises").value
+    if (Paises =="") {
+        alert("Seleccione un país")
+        return
     }
 }
